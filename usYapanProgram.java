f600d2a3c6f04796b35ae782031012109046b026
Vusal Gurbanov <98140799+Vusal-Gurbanov@users.noreply.github.com>
2022-10-15 06:01:44 +0300

    usYapanProgram
    UsYapanProgram

---
+++ usYapanProgram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+//Java dilinde, taban ve üs değerleri kullanıcıdan alınan üs alma işlemini "Recursive" metot kullanarak yapan programı yazınız.
+
+public class usYapanProgram {
+    static int power(){
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Üssü alınacak sayı: ");
+        int temel= inp.nextInt();
+        System.out.println("Üs olacak sayı: ");
+        int uss= inp.nextInt();
+
+        int sonuc=1;
+        for (int i=1;i<=uss;i++){
+            sonuc*=temel;
+            if (uss==0){
+                return 1;
+            }
+
+        }
+        System.out.println("Result:"+sonuc);
+        return sonuc;
+    }
+
+    public static void main(String[] args) {
+        power();
+    }
+}
+/*
+*
+*
+* */
